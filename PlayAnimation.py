2bb67392039a5607551c2fe19b0261f5707ba4cd
k-klaudia0 <kristofnapsugar@gmail.com>
2023-05-04 22:06:34 +0200

    commit

---
+++ PlayAnimation.py
@@ -0,0 +1,19 @@
+import wx
+import random
+from wx.adv import Animation, AnimationCtrl
+
+class PlayAnimation(wx.Frame):
+        def __init__(self, title):
+            xPos = random.randint(0,1024)
+            yPos = random.randint(0,768)
+            super().__init__(parent = None, title = title, size = (502,263), pos = (xPos, yPos))
+            self.panel = wx.Panel(self)
+
+            wrapper = wx.BoxSizer(wx.VERTICAL)
+            sizer = wx.FlexGridSizer(rows = 2, cols = 1, vgap = 5, hgap = 5)
+
+            wrapper.Add(sizer, proportion = 1, flag = wx.ALL | wx.EXPAND, border = 10)
+
+            anim = Animation("gif2.gif")
+            anim_ctrl = AnimationCtrl(self.panel, wx.ID_ANY, anim)
+            anim_ctrl.Play()
